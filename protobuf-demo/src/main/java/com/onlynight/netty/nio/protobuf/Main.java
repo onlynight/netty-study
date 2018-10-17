6ff8e16d944edf0f1dbfe80b73c6022892467d8e
wyndam <only.night@qq.com>
2018-10-17 10:27:55 +0800

    add protobuf demo

--- protobuf-demo/src/main/java/com/onlynight/netty/nio/protobuf/Main.java
+++ protobuf-demo/src/main/java/com/onlynight/netty/nio/protobuf/Main.java
@@ -12,10 +12,20 @@ public class Main {
             DemoProtos.Pet pet0 = DemoProtos.Pet.newBuilder().setId(0).setName("小脑斧0").build();
             DemoProtos.Pet pet1 = DemoProtos.Pet.newBuilder().setId(1).setName("小脑斧1").build();
             DemoProtos.Pet pet2 = DemoProtos.Pet.newBuilder().setId(2).setName("小脑斧5").build();
+            DemoProtos.Pet pet3 = DemoProtos.Pet.newBuilder().setId(3).setName("小脑斧7").build();
+            DemoProtos.Pet pet4 = DemoProtos.Pet.newBuilder().setId(4).setName("小脑斧6").build();
+            DemoProtos.Pet pet5 = DemoProtos.Pet.newBuilder().setId(5).setName("小脑斧4").build();
+            DemoProtos.Pet pet6 = DemoProtos.Pet.newBuilder().setId(6).setName("小脑斧3").build();
+            DemoProtos.Pet pet7 = DemoProtos.Pet.newBuilder().setId(7).setName("小脑斧2").build();
             DemoProtos.Person person = DemoProtos.Person.newBuilder()
                     .addPet(pet0)
                     .addPet(pet1)
                     .addPet(pet2)
+                    .addPet(pet3)
+                    .addPet(pet4)
+                    .addPet(pet5)
+                    .addPet(pet6)
+                    .addPet(pet7)
                     .build();
             person.writeTo(os);
             os.close();
